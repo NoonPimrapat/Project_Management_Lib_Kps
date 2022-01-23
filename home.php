1304a730b476c823ed45983c59cbd4b3481c78d3
NoonPimrapat <pimrapat.t@ku.th>
2022-01-23 22:35:02 +0700

    อะไรก็ได้

---
+++ home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;600&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width ,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/style.css">
+
+  <title>
+        สำนักงานหอสมุดกำแพงแสน
+    </title>
+</head>
+
+<body>
+    <div class="logo-container">
+        <div class="logo">
+            <img src="img/ku.jpg" alt="logo ku"class="mini-logo">
+            <img src="img/ku_logo.jpg" alt="logo ku"class="mini-logo-ku" >
+        </div>
+        <div class="profile-logo">
+            <img src="img/ku.jpg" alt="logo ku"class="profile">
+            <p>Pimrapat</p>
+            <div class="triangleBottom" onclick="myFunction()></div>
+            
+        </div>
+    </div>
+    <div class="logo-container">
+        <div class="color-bar">
+           <p class="title">แผนปฎิบัติการประจำปีงบประมาณ ปัจจุบันoo </p>
+        </div>
+        
+      
+    </div>
+    <div class="grid-container">
+        <button onclick="myFunction()" class="menuButton">   1. ขออนุมัติโครงการ</button>
+        <button onclick="myFunction()" class="menuButton">4. ขออนุมัติเบิก-จ่าย รายครั้ง</button>
+        <button onclick="myFunction()" class="menuButton">2. รายงานผลการดำเนินงาน</button>
+        <button onclick="myFunction()" class="menuButton">   5. ขอนุมัติปิดโครงการ</button>
+        <button onclick="myFunction()" class="menuButton">3. ขออนุมัติปรับแผนโครงการ</button>
+        <button onclick="myFunction()" class="menuButton">6. สรุปการดำเนินงานตามแผน</button>
+        <button onclick="myFunction()" class="menuButton">7. แก้ไข/ตรวจสอบสถานะโครงการ</button>
+    </div>
+    <div class="logo-container">
+        <div class="color-bar">
+           <p class="title">แผนปฎิบัติการประจำปีงบประมาณ พ.ศ.  </p>
+        </div>
+
+    </div>
+    <div class="container">
+       
+        <div >
+            <h2 >แบบสรุปรายงานผลการดำเนินงาน</h2>
+            <h2 >ตามแผนปฎิบัติการประจำปี งบประมาณ ......</h2>
+        </div>
+        <h2 class="subTitle">รายงานผลการดำเนินงานในรอบไตรมาส ........</h2>
+        <button onclick="myFunction()" class="menuButton2">รายงานโครงการตามแผนงานประจำ</button>
+    </div>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;600&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width ,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/style.css">
+
+  <title>
+        สำนักงานหอสมุดกำแพงแสน
+    </title>
+</head>
+
+<body>
+    <div class="grid-container">
+        <div class="grid-item"><img src="img/display.jpg" alt="logo ku"
+        class="imgDisplay">
+        </div>
+        <div class="grid-item">
+            <img src="img/ku_logo.jpg" alt="logo ku"class="logoDisplay" >
+
+            <h2 class="textTitle">
+            เข้าสู่ระบบด้วยอีเมลผู้ใช้งาน </h2>
+            <form action="/action_page.php">
+                <label for="fname">E-mail:</label><br>
+                <input type="email" id="fname" name="fname" value="E-mail" class="inputFill"><br>
+                <label for="lname"  >Password:</label><br>
+                <input type="password" id="lname" name="lname" value="Password" class="inputFill"><br><br>
+                <input type="submit" value="Login" class="summitButton">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
